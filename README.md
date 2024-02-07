7633975bcf6257b1c53b4aa1a8a88f8dee426354
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-02-07 15:12:42 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # php-training
+
+1. Variables
+- variable declaration
+- data types
+- debugging functions (var_dump() and die())
+- constant and define()
+- type casting
+- type juggling
--- helloworld.php
+++ helloworld.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Variables</title>
+    <title>Main</title>
 </head>
 <body>
     <!-- combining the php and the html  -->
