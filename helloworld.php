aecbc984fdee1b24d70a4820ea7fc5f7e3a5aec2
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-02-10 23:12:14 +0800

    functions not done yet

--- helloworld.php
+++ helloworld.php
@@ -8,19 +8,48 @@
 <!-- the var should always be declared at the top -->
 <?php
     $name = "Melares";
+
+    // function syntax
+    // function functionName($parameter){
+    //     statement;
+    // }
+
+    function hello($myName){
+        echo 'Hello '.$myName;
+    }
+
+    // function returnFunction(parameter){
+    //     return statement/value;
+    // }
+    function returnFunction($myName){
+        return 'Hello '. $myName; 
+    }
+
+
 ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main</title>
+    <title>Functions</title>
 </head>
 <body>
     <!-- combining the php and the html  -->
     <h1>
-        <?= "Main";?>
+        <?= "Functions";?>
     </h1>
     <h2>
-        
+        Function Declaration
+    <?php
+    echo '<pre>';
+    // declaring function syntax
+    // functionName(argument);
+    hello($name);
+    // the returnFunction is assigining a string value to $helloName
+    $helloName = returnFunction($name);
+    echo "\n";
+    echo $helloName;
+    echo '</pre>';
+    ?>
     </h2>
 </body>
 </html>
