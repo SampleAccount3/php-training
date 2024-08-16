b4c26597168f617262d461509602ec70629139d9
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-08-16 13:57:37 +0800

    Init Commit

--- helloworld.php
+++ helloworld.php
@@ -1,23 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-<!-- the var should always be declared at the top -->
 <?php
+    include "./TypeHints.php";
     $name = "Melares";
+    $result= null;
+    $sample_object = new Sample();
 ?>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <!-- combining the php and the html  -->
-    <h1>
-        Kangkong
-        <?php
-            echo "Hello World!!! $name";
-        ?>
-    </h1>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>
+            <!-- Simple Method Call -->
+            <?php
+                echo $sample_object->show_hello(); 
+            ?>
+        </h1>
+        <h1>
+            <!-- Class reference / Method Reference-->
+            <?php
+                echo Sample::static_show_hello(); 
+            ?>
+        </h1>
+        <!-- Different Data Types -->
+        <h1><?php echo $result = Sample::dynamic_type_sample(1,2);?></h1>
+        <h1><?php echo $result = Sample::dynamic_type_sample(1.0,2.2);?></h1>
+        <h1><?php echo $result = Sample::dynamic_type_sample(true,false);?></h1>
+        <h1><?php echo $result = Sample::dynamic_type_sample("1","2");?></h1>
+
+        <!-- You can specify the data types of parameters -->
+        <h1><?php echo $result = Sample::int_type_sample(2,5);?></h1>
+        <h1><?php echo var_dump($result = Sample::string_type_sample("1","66"));?></h1>
+
+    </body>
 </html>
-
-
