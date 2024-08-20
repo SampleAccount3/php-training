55298b8060617e4885bce26c4763084f976cfd66
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-08-20 16:16:21 +0800

    Init Commit

--- Collections.php
+++ Collections.php
@@ -1,4 +1,9 @@
 <?php
+
+// Use the array() construct or [] syntax to create a new array.
+// For the indexed array, the first index begins with zero.
+// To access an array element, use an index in the square bracket $array_name[index].
+// Use the count() function to get the number of elements in an array.
     class Test1{
         private static $sample = "Hello";
 
@@ -20,7 +25,6 @@
 
             // print_r(self::$arrSample);
         }
-
         public static function accessingArrayElements($index){
          
             return self::$arrSample[$index];//2
@@ -32,9 +36,57 @@
         public static function updatingArrayElements(){
             self::$arrSample[0] = 9;
         }
+        public static function removingArrayElement(){
+            unset(self::$arrSample[3]); // 4
+        }
+        public static function countingTheArrayElement(){
+           return count(self::$arrSample);
+        }
 
-
+        
     }
-    
+    class CollectionAssocArray{
+        private static $assoArraySample = array();
+        private static $assocArrayWithElements = [
+            'Japan' => 'Tokyo',
+            'France' => 'Paris',
+            'Germany' => 'Berlin',
+            'United Kingdom' => 'London',
+            'United States' => 'Washington D.C.'
+        ];
 
+        public static function addingElementsInAssocArray(){
+            self::$assoArraySample['Name'] = "Melares";
+            self::$assoArraySample['Job'] = "Programmer";
+            self::$assoArraySample['Age'] = "21";
+            self::$assoArraySample['Sample'] = "SampleTest";
+
+            print_r(self::$assoArraySample);
+        }
+        public static function accessingAssocArrayElement($key){
+            echo self::$assoArraySample[$key]; 
+        }
+        public static function forEachAssocArray(){
+            // iterating the AssocArray
+            foreach(self::$assoArraySample as $key => $value){
+                echo "$key  $value" . '<br>';
+            }
+        }
+        public static function forEachAssocArray2(){
+            // iterating the AssocArray
+            foreach(self::$assocArrayWithElements as $key => $value){
+                echo "$key  $value" . '<br>';
+            }
+        }
+    }
+
+    class ArrayMethods{
+        private static $sampleArray = ["A","B","C","D","E","F","G","H"];
+        public static function printArray(){
+            print_r(self::$sampleArray);
+        }
+    }
+
+
+    
 ?>
--- helloworld.php
+++ helloworld.php
@@ -18,7 +18,7 @@
 <body>
     <!-- combining the php and the html  -->
     <h1>
-        <?= "Main";?>
+        <?="Main";?>
     </h1>
     <h1><?=Test1::testFunction()?></h1>
     <h2></h2>
@@ -28,6 +28,19 @@
     <h2>After adding the Array: <?=Collections::normalArray()?></h2>
     <h2>Updating the array Elements<?= Collections::updatingArrayElements()?></h2>
     <h2>After updating the Array:<?=Collections::normalArray()?></h2>
-
+    <h2>Removed the array Element(index 3) 4<?= Collections::removingArrayElement()?></h2>
+    <h2>After Removing the array Elements<?= Collections::normalArray()?></h2>
+    <h2>The total count of Array Elements: <?= Collections::countingTheArrayElement()?></h2>
+    <h1>Associative arrays </h1>
+    <h2><?= CollectionAssocArray::addingElementsInAssocArray()?></h2>
+    <h2>Accessing Associative Array: </h2>
+    <h2>Accessing Name: <?= CollectionAssocArray::accessingAssocArrayElement("Name")?></h2>
+    <h2>Accessing Job: <?= CollectionAssocArray::accessingAssocArrayElement("Job")?></h2>
+    <h2>Accessing Age: <?= CollectionAssocArray::accessingAssocArrayElement("Age")?></h2>
+    <h2>Accessing Sample: <?= CollectionAssocArray::accessingAssocArrayElement("Sample")?></h2>
+    <h2>For Each Assoc Array: </br> <?= CollectionAssocArray::forEachAssocArray()?>
+    <h2>For Each Assoc With Elements: </br> <?= CollectionAssocArray::forEachAssocArray2()?>
+    <h1>Array Methods</h1>
+    <h2><?= ArrayMethods::printArray()?></h2>
 </body>
 </html>
